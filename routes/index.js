cff323daf2c8397dd3cc34ea32120d201022e1ff
Jeremy <howard55662004@yahoo.com>
2016-09-07 22:50:16 +0800

    add models

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,72 @@
-var express = require('express');
-var router = express.Router();
+var crypto = require('crypto'),
+	User = require('../models/user.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(app) {
+	app.get('/', function (req, res){
+		res.render('index', { 
+			title: 'Home',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+	app.get('/reg', function (req, res){
+		res.render('reg', { 
+			title: 'Register',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+	app.post('/reg', function (req, res){
+		var name = req.body.name,
+			password = req.body.password,
+			password_re = req.body['password-repeat'];
+		if (password_re != password){
+			req.flash('error', 'The confirm password is not correct!');
+			return res.redirect('/reg');
+		}
 
-module.exports = router;
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+		var newUser = new User({
+			name: req.body.name,
+			password: password,
+			email: req.body.email
+		});
+
+		//check if the user already existed
+		User.get(newUser.name, function(err, user){
+			if(user){
+				req.flash('error', 'User already existed!');
+				return res.redirect('/reg');
+			}
+			//if not exist then add new user
+			newUser.save(function(err, user){
+				if(err){
+					req.flash('error', err);
+					return res.redirect('/reg');
+				}
+				req.session.user = user;  // store information of user in session
+				req.flash('success', 'Register successfully !');
+				res.redirect('/');
+			});
+		});
+	});
+
+	app.get('/login', function (req, res){
+		res.render('login', { title: 'Login'});
+	});
+	app.post('/login', function(req,res){
+
+	});
+	app.get('/post', function (req, res){
+		res.render('post', { title: 'Post'});
+	});
+	app.post('/post', function (req, res){
+
+	});
+	app.post('/logout', function (req, res){
+
+	});
+};
