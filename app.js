157c8059793c7aa5be3ab21aee71a1a9c2f6dba2
Jeremy <howard55662004@yahoo.com>
2016-09-07 22:52:10 +0800

    add modules

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var settings = require('./settings');
+var flash = require('connect-flash');
+
+var fs = require('fs');
 
 var app = express();
 
 // view engine setup
+//app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -19,12 +25,33 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,                             //cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},   //30 days
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port: settings.port
+  })
+}));
+
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
+routes(app);
+
+/*
+app.listen(app.get('port'), function(){
+  console.log('Express server listening port' + app.get('port'));
+});
+*/
+
+module.exports = app;                           //exports app especially for bin/www
+/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -56,5 +83,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
 module.exports = app;
+*/
