4a8852856593de99253f971502d6ce08b11012b1
Jeremy <howard55662004@yahoo.com>
2016-09-19 14:12:38 +0800

    use local database

--- settings.js
+++ settings.js
@@ -1,11 +1,6 @@
-var db_username = process.env.YOUR_DB_USERNAME;
-var db_password = process.env.YOUR_DB_PASSWORD;
-
-
 module.exports = {
 	cookieSecret: 'myblog',
-	//db: 'blog',
-	//host: 'localhost',
-	//port: 27017
-	url: 'mongodb://'+ db_username + ':' + db_password + '@ds033076.mlab.com:33076/blog_project'
+	db: 'blog',
+	host: 'localhost',
+	port: 27017
 };
